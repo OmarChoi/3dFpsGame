2a4cf0281d6dd8a8d4944e8c62899cdcb0ef6d86
Omarchoi <omarchoi80@gmail.com>
2025-12-08 15:23:31 +0900

    feat: 마우스 Input에 따른 카메라 회전 기능 추가

---
+++ Assets/02.Scripts/CameraRotate.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed;
+    
+    // 유니티는 0 ~ 360 각도 체계이므로 우리가 따로 저장할 -360 ~ 360 체계로 누적할 변수
+    private float _accumulationX;
+    private float _accumulationY;
+    
+    private void Update()
+    {
+        if (!Input.GetMouseButton(1)) return;
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        
+        _accumulationX += mouseX * _rotationSpeed * Time.deltaTime;
+        _accumulationY += -mouseY * _rotationSpeed * Time.deltaTime;
+        _accumulationY = Mathf.Clamp(_accumulationY, -90f, 90f);
+        
+        transform.eulerAngles = new Vector3(_accumulationY, _accumulationX, 0);
+    }
+}
