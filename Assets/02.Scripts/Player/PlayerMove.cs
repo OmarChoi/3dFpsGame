a45ff4b3f620658cc2896801612886281e4e0cb2
Omarchoi <omarchoi80@gmail.com>
2025-12-08 16:50:48 +0900

    feat: 중력 적용 및 점프 기능 추가

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float _movementSpeed = 7.0f;
     private CharacterController _controller;
-
+    
+    [SerializeField] private float _movementSpeed = 7.0f;
+    
+    private readonly float _gravity = -9.81f;
+    private float _yVelocity = 0.0f;    // 중력에 의해 누적될 y값 변수
+    [SerializeField] private float _jumpPower = 5.0f;
+    
     private void Awake()
     {
         _controller = GetComponent<CharacterController>();
@@ -13,12 +18,21 @@ public class PlayerMove : MonoBehaviour
     
     private void Update()
     {
+        _yVelocity += _gravity * Time.deltaTime;
+        
         float xMovement = Input.GetAxis("Horizontal");
         float zMovement = Input.GetAxis("Vertical");
         
         Vector3 direction = new Vector3(xMovement, 0, zMovement);
         direction.Normalize();
+
+        if (Input.GetButtonDown("Jump") && _controller.isGrounded)
+        {
+            _yVelocity = _jumpPower;
+        }
+        
         direction = Camera.main.transform.TransformDirection(direction);
+        direction.y = _yVelocity;
         
         _controller.Move(direction * (_movementSpeed * Time.deltaTime));
     }
