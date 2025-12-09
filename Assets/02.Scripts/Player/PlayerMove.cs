6c368d35ef5a44fe0133ac1aa68e9f89700b834c
Omarchoi <omarchoi80@gmail.com>
2025-12-09 10:56:15 +0900

    feat: Player 2단 점프 구현

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -18,8 +18,10 @@ public class PlayerMove : MonoBehaviour
     [Space]
     [SerializeField] private float _jumpPower;
     [SerializeField] private float _jumpStaminaUsage;
+    [SerializeField] private float _maxJumpCount;
+    private int _jumpCounter;
     private const float Gravity = -9.81f;
-    private float _yVelocity = 0.0f;    // 중력에 의해 누적될 y값 변수
+    private float _yVelocity;    // 중력에 의해 누적될 y값 변수
     
     private void Awake()
     {
@@ -30,28 +32,25 @@ public class PlayerMove : MonoBehaviour
     
     private void Update()
     {
+        Jump();
         Move();
     }
 
     private float GetSpeed()
     {
-        float speed = _movementSpeed;
-        if (Input.GetKey(KeyCode.LeftShift) && _stat.TryUseStamina(_runStaminaUsage * Time.deltaTime))
-        {
-            speed = _runningSpeed;
-        }
-        return speed;
+        bool canRun = Input.GetKey(KeyCode.LeftShift) && 
+                      _stat.TryUseStamina(_runStaminaUsage * Time.deltaTime);
+        return canRun ? _runningSpeed : _movementSpeed;
     }
     
     private void Move()
     {
         float movementSpeed = GetSpeed();
-        float yVelocity = GetYVelocity();
-        Vector3 direction = GetDirection(yVelocity);
+        Vector3 direction = GetDirection();
         _controller.Move(direction * (movementSpeed * Time.deltaTime));
     }
 
-    private Vector3 GetDirection(float yVelocity)
+    private Vector3 GetDirection()
     {        
         float xMovement = Input.GetAxis("Horizontal");
         float zMovement = Input.GetAxis("Vertical");
@@ -59,17 +58,29 @@ public class PlayerMove : MonoBehaviour
         Vector3 direction = new Vector3(xMovement, 0, zMovement);
         direction.Normalize();
         direction = _mainCamera.transform.TransformDirection(direction);
-        direction.y = yVelocity;
+        direction.y = _yVelocity;
         return direction;
     }
     
-    private float GetYVelocity()
+    private void Jump()
     {
         _yVelocity += Gravity * Time.deltaTime;
-        if (Input.GetButtonDown("Jump") && _controller.isGrounded)
+        if (!Input.GetButtonDown("Jump")) return;
+
+        if (_controller.isGrounded)
         {
+            _jumpCounter = 1;
+            _yVelocity = _jumpPower;
+            return;
+        }
+
+        bool canAirJump = _jumpCounter < _maxJumpCount 
+                          && _stat.TryUseStamina(_jumpStaminaUsage);
+
+        if (canAirJump)
+        {
+            _jumpCounter++;
             _yVelocity = _jumpPower;
         }
-        return _yVelocity;
     }
 }
