ae2732c24b43951f23bb8746fd4fc09f832cf731
Omarchoi <omarchoi80@gmail.com>
2025-12-09 18:21:20 +0900

    feat: 폭탄 개수 설정 및 UI 표시

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -5,14 +5,17 @@ public class PlayerFire : MonoBehaviour
     [SerializeField] private Transform _fireTransform;
     [SerializeField] private Bomb _bombPrefab;
     [SerializeField] private float _throwPower = 15f;
-    
+    private int _bombCount = 5;
+    public int BombCount => _bombCount;
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1) && _bombCount > 0)
         {
+            _bombCount--;
             Bomb bomb = Instantiate(_bombPrefab, _fireTransform.position, Quaternion.identity);
             Rigidbody rigidbody = bomb.GetComponent<Rigidbody>();
-            rigidbody.AddForce(Camera.main.transform.forward * _throwPower, ForceMode.Impulse);
+            rigidbody?.AddForce(Camera.main.transform.forward * _throwPower, ForceMode.Impulse);
         }
     }
 }
--- Assets/02.Scripts/UI/StatsUI.cs
+++ Assets/02.Scripts/UI/StatsUI.cs
@@ -1,27 +1,37 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class StatsUI : MonoBehaviour
 {
     private PlayerStats _stats;
+    private PlayerFire _playerFire;
     
     [Header("UI")]
     [Space]
     [SerializeField] private Slider _healthBar;
     [SerializeField] private Slider _staminaBar;
-
+    [SerializeField] private TextMeshProUGUI _bombCount;
     private void Awake()
     {
         _stats = FindFirstObjectByType<PlayerStats>();
+        _playerFire = FindFirstObjectByType<PlayerFire>();
     }
 
     private void Update()
     {
         UpdateStaminaUI(_stats.Stamina.Value, _stats.Stamina.MaxValue);
+        UpdateBombCount();
     }
     
     private void UpdateStaminaUI(float stamina, float maxStamina)
     {
         _staminaBar.value = stamina / maxStamina;
+    }
+
+    private void UpdateBombCount()
+    {
+        int count = _playerFire.BombCount;
+        _bombCount.text = $"x{count}";
     }
 }
