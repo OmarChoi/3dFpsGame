abc51d08b4ec5ff0c5ff9090c0bab49dbaadfe8a
Omarchoi <omarchoi80@gmail.com>
2025-12-15 10:06:52 +0900

    feat: Zombie 체력 UI 추가

---
+++ Assets/02.Scripts/Zombie/ZombieHealthBar.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Zombie))]
+public class ZombieHealthBar : MonoBehaviour
+{
+    private Zombie _zombie;
+    [SerializeField] private Image _guageImage;
+    [SerializeField] private Transform _healthBarTransform;
+    private Camera _mainCamera;
+    private float _lastHealth = -1;
+    private void Awake()
+    {
+        _zombie = GetComponent<Zombie>();
+        _mainCamera = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        float currentHealth = _zombie.Health.Value / _zombie.Health.MaxValue;
+        if (!Mathf.Approximately(_lastHealth, currentHealth))
+        {
+            _guageImage.fillAmount = currentHealth;
+            _lastHealth = currentHealth;
+        }
+        
+        
+        _healthBarTransform.forward = _mainCamera.transform.forward;
+    }
+}
